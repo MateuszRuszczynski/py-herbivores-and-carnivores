4bdbc6bea55baef4d1b8d8baf06e077ca18e34a2
Mateusz <mateusz.ruszczynski@gmail.com>
2025-10-21 21:53:34 +0200

    Solution for herbivores and carnivores

--- app/main.py
+++ app/main.py
@@ -1,1 +1,37 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+
+        Animal.alive.append(self)
+
+    def die_if_needed(self) -> None:
+        if self.health <= 0:
+            self.health = 0
+            Animal.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+
+    def bite(self, other: Animal) -> None:
+        if not isinstance(other, Herbivore):
+            return
+        if other.hidden:
+            return
+
+        other.health = max(0, other.health - 50)
+        other.die_if_needed()
